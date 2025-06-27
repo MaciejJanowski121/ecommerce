dcad4e5621111da2c61c5add8ba8cc501b78c342
maciejjanowski <maciej.janowski7@hotmail.com>
2025-06-27 10:17:12 +0200

    Add ProductController with REST endpoint

---
+++ src/main/java/org/example/ecommerce/controller/ProductController.java
@@ -0,0 +1,23 @@
+package org.example.ecommerce.controller;
+
+import org.example.ecommerce.model.Product;
+import org.example.ecommerce.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return service.getAllProducts();
+    }
+}
